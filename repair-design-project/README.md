5b31ff50762dcdaa1e4a3b679ee1e5853a162889
VladislavRb <vlad.raba@mail.ru>
2019-12-07 09:23:29 +0300

    add: repair-design-project copy

---
+++ repair-design-project/README.md
@@ -0,0 +1,4 @@
+# repair-design-project
+repair-design-project first draft
+
+Clone or download the repository and go to the repair-design-project folder. Write npm-install and then npm run build. After that, open index.html, go to terminal and write npm start
---
+++ repair-design-project/webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg|woff(2)?|ttf|otf)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.90],
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75
+              }
+            }
+          },
+        ],
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css')
+  ]
+};
